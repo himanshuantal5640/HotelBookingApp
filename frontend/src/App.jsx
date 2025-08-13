221ad76b114309e47735465b529a4ccb30bfda14
himanshu-antal <himanshuantal26@gmail.com>
2025-08-13 12:15:12 +0530

    footer done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -11,8 +12,10 @@ const App = () => {
      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>} />
+
         </Routes>
      </div>
+     <Footer/>
     </div>
   )
 }

7b188fe5ca7a8f2ac71dbe973471416287b2464c
himanshu-antal <himanshuantal26@gmail.com>
2025-08-14 01:08:41 +0530

    added

---
+++ frontend/src/pages/AllRooms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllRooms = () => {
+  return (
+    <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 '>
+      <h1>All Rooms</h1>
+    </div>
+  )
+}
+
+export default AllRooms

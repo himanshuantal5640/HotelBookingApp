f1efc89f987ff428de74514df7cdb7220377c4d2
himanshu-antal <himanshuantal26@gmail.com>
2025-08-22 11:44:05 +0530

    updation

--- frontend/src/pages/AllRooms.jsx
+++ frontend/src/pages/AllRooms.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  assets, roomsDummyData } from '../assets/assets'
+import {  assets, facilityIcons, roomsDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import StarRating from '../components/StarRating'
 
@@ -15,7 +15,8 @@ const AllRooms = () => {
         </div>
 
         {roomsDummyData.map((room)=>(
-          <div>
+          <div key={room._id} className='flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 
+          last:border-0 '>
             <img onClick={()=> {navigate(`/rooms/${room._id}`);scrollTo(0,0)}}
              src={room.images[0]} alt="hotel-img" title='View Room Details' className='max-h-65
              md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer '  />
@@ -30,13 +31,31 @@ const AllRooms = () => {
                 <img src={assets.locationIcon} alt="location-icon" />
                 <span>{room.hotel.address}</span>
               </div>
+              {/* room ammenities */}
+              <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
+                {room.amenities.map((item,index)=>(
+                  <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg 
+                  bg-[#F5F5FF]/70 '>
+                    <img src={facilityIcons[item]}  alt={item} className='w-5 h-5' />
+                    <p className='text-xs'>{item}</p>
+                  </div>
+                ))}
+              </div>
+              {/* room price */}
+              <p className='text-xl font-medium text-gray-700'>Rs.{room.pricePerNight}/night</p>
             </div>
           </div>
         ))}
       </div>
       {/* Filter */}
-      <div>
-
+      <div className='bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16'>
+          <div>
+            <p className='text-base font-medium text-gray-800'>FILTERS</p>
+            <div className='text-xs cursor-pointer'>
+              <span className='lg:hidden'>HIDE</span>
+              <span className='hidden lg:block'>CLEAR</span>
+            </div>
+          </div>
       </div>
     </div>
   )

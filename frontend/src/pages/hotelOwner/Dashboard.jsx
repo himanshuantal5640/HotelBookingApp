daf02dd76059fc0a6174728c2a1c0439a6d46502
himanshu-antal <himanshuantal26@gmail.com>
2025-09-06 15:32:14 +0530

    frontend updation

--- frontend/src/pages/hotelOwner/Dashboard.jsx
+++ frontend/src/pages/hotelOwner/Dashboard.jsx
@@ -47,6 +47,17 @@ const Dashboard = () => {
                                 <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
                                     {item.user.username}
                                 </td>
+                                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden'>
+                                    {item.room.roomType}
+                                </td>
+                                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
+                                    Rs. {item.totalPrice}
+                                </td>
+                                <td className='py-3 px-4 border-t border-gray-300 flex'>
+                                    <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
+                                        {item.isPaid ? 'Completed' : 'Pending'}
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                 </tbody>

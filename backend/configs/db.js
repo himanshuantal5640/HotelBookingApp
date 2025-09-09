aa2164afb34a5a84cbf9858ec325e1f6c7510474
himanshu-antal <himanshuantal26@gmail.com>
2025-09-09 14:29:36 +0530

    user model created

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     mongoose.connection.on("connected", () => {
-      console.log("✅ Connected to MongoDB");
+      console.log("Connected to MongoDB");
     });
     await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
-    console.error("❌ DB Connection Failed:", error.message);
+    console.error("DB Connection Failed:", error.message);
   }
 };
 

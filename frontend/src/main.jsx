6ef53ca491896816bb4cf66b8ff9df6a223c31ff
himanshu-antal <himanshuantal26@gmail.com>
2025-10-10 13:32:25 +0530

    hotelroom backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { AppProvider } from './context/AppContext.jsx'
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -11,9 +12,11 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl= "/">
   <BrowserRouter>
+  <AppProvider>
     <App />
+  </AppProvider>
   </BrowserRouter>
   </ClerkProvider>,
 )
